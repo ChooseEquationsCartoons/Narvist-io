1aafb243bd9ff49b29ffd6c3aa77214b9a7aae60
Glitch (ossified-understood-individual) <none>
2023-05-02 21:12:51 +0000

    🚜🍽 Checkpoint ./server/lib/definitions.js:57091253/2451

--- server/lib/definitions.js
+++ server/lib/definitions.js
@@ -6394,9 +6394,9 @@ SKILL_CAP: [metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl
          }, }, 
      ],
 };
-exports.pastra = {
+exports.pastralest = {
    PARENT: [exports.genericTank],
-   LABEL: 'Pastra',
+   LABEL: 'Pastra-Lest',
    SHAPE: 6,
   COLOR: 9,
    SIZE: 80,
@@ -6425,7 +6425,21 @@ SKILL_CAP: [metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl
          PROPERTIES: {
             SHOOT_SETTINGS: combineStats([g.basic]),
             TYPE: exports.autoswarm,
-         }, }, 
+           },
+    }],
+  TURRETS: [
+    {
+      /******  SIZE      X       Y     ANGLE    ARC  LAYER */
+      POSITION: [8, 5, 0, 0, 0, 1],
+      TYPE: [
+        exports.auto3gun],
+    },
+    {
+        POSITION: [8, -5, 0, 0, 0, 1],
+      TYPE: [
+        exports.auto3gun],
+
+         },
      ],
 };
 exports.pastramullybox = {
@@ -6543,10 +6557,49 @@ SKILL_CAP: [metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl
          }, }, 
      ],
 };
+exports.yelest = {
+   PARENT: [exports.genericTank],
+   LABEL: 'Yelest',
+   SHAPE: 6,
+  COLOR: 9,
+   SIZE: 80,
+  COLOR: 9,
+  VALUE: 1999000,
+   CONTROLLERS: [
+        'nearestDifferentMaster', 'mapAltToFire', 'minion', 'fleeAtLowHealth'],
+SKILL_CAP: [metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl, metroskl],
+  BODY: {
+  HEALTH: 2000,
+  SHIELD: 100,
+  SPEED: 1.5,
+  },
+   GUNS: [ {
+        POSITION: [15, 7, 1, 0, 0, 0, 0]
+    }, {
+        POSITION: [3, 7, 1.7, 15, 0, 0, 0],
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.trap]),
+            TYPE: exports.trap,
+            STAT_CALCULATOR: gunCalcNames.trap
+        },
+    },
+          {
+             POSITION: [15, 7, 1, 0, 0, 121.5, 0]
+    }, {
+        POSITION: [3, 7, 1.7, 15, 0, -122, 0],
+        PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.trap]),
+            TYPE: exports.trap,
+            STAT_CALCULATOR: gunCalcNames.trap
+
+
+         }, }, 
+     ],
+};
 exports.testbed.UPGRADES_TIER_1 = [exports.betaTanks, exports.bosses, exports.misc, exports.basic, exports.lestie];
 exports.lestie.UPGRADES_TIER_1 = [exports.Metronaught1, exports.tabatay, exports.pastra];
 exports.tabatay.UPGRADES_TIER_1 = [exports.tabatayfloss, exports.tabatayblenc];
-exports.pastra.UPGRADES_TIER_1 = [exports.pastramullybox, exports.pastralashes];
+exports.pastra.UPGRADES_TIER_1 = [exports.pastramullybox, exports.pastralashes, exports.pastralest];
 exports.Metronaught1.UPGRADES_TIER_1 = [exports.Metronaught2, exports.Metronaughtrulfle, exports.faracnolo, exports.likiloak];
 exports.felecimetro.UPGRADES_TIER_1 = [exports.liacarapooi];
 exports.faracnolo.UPGRADES_TIER_1 = [exports.aliniloko, exports.felecimetro, exports.Metronaught3, exports.halacaust, exports.gevericnomo];
